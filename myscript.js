7bd226b2349107d7698b02aa1442b65685d1c230
Edson-Batista <edsonufrjx@gmail.com>
2021-09-04 22:34:50 -0300

    Renomeei algumas imagens e alterei a page principal

---
+++ myscript.js
@@ -0,0 +1,12 @@
+document.onreadystatechange = function () {
+    var state = document.readyState
+    if (state == 'interactive') {
+         document.getElementById('video').style.visibility="hidden";
+    } else if (state == 'complete') {
+        setTimeout(function(){
+           document.getElementById('interactive');
+           document.getElementById('load').style.visibility="hidden";
+           document.getElementById('video').style.visibility="visible";
+        },50);
+    }
+  }
